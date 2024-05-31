20e81165614938085678788330d93163d31d72e1
Jason Hazel <me@jasonhazel>
2024-05-30 21:51:00 -0400

    quick add of bw:pocket

---
+++ keyboards/hazel/bw_pocket/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Jason Hazel (@jasonhazel)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xF7, 0x3A, 0x77, 0x47, 0x2F, 0xF8, 0x10, 0x42}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4}
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
